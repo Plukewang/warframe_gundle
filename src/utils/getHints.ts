f1cf2f091faba1730827ca8f2d3ef07d05f68b9e
Peiyang Wang <plukewang@gmail.com>
2025-04-11 14:08:57 -0500

    remove junk data from randomWeapons

---
+++ src/utils/getHints.ts
@@ -0,0 +1,12 @@
+import { weaponData } from "../types/weaponData";
+
+export default function getHints(randomWeapon: weaponData){
+    switch(randomWeapon.productCategory){
+        case "longGuns":
+            break;
+        case "Pistols":
+            break;
+        case "Melee":
+            break;
+    }
+}
--- src/utils/randomWeapon.ts
+++ src/utils/randomWeapon.ts
@@ -1,21 +1,24 @@
 import weaponsData from '../data.json'
-import {weaponData, gunData} from '../types/weaponData';
+import {weaponData} from '../types/weaponData';
 /**
  * Array-fy the data.json and spit out a random weapon.
+ * 
  */
 
 function getRandomWeapon():weaponData{
+    const endPoint = 599;
     const weaponData = Object.entries(weaponsData.ExportWeapons);
-    const arrLen = weaponData.length;
 
     //make a random int of < arrLen
-    const randomWeapon: weaponData = weaponData[Math.floor(Math.random() * arrLen)][1];
+    const randomWeapon = weaponData[Math.floor(Math.random() * endPoint)][1];
     //console.log(<gunData>randomWeapon);// 2nd element is the weapon object.
-
+    console.log(randomWeapon)
     return randomWeapon;
 }
 
 
 
 getRandomWeapon()
+
+
 
